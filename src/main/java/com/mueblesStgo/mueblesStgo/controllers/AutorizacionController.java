d35256d9737ceade741f6cbc310a20075e1a8c30
Angel Vilches U <angel.vilches@usach.cl>
2022-11-07 21:54:59 -0300

    Autorización POST

--- src/main/java/com/mueblesStgo/mueblesStgo/controllers/AutorizacionController.java
+++ src/main/java/com/mueblesStgo/mueblesStgo/controllers/AutorizacionController.java
@@ -2,6 +2,8 @@ package com.mueblesStgo.mueblesStgo.controllers;
 
 import com.mueblesStgo.mueblesStgo.entities.AutorizacionEntity;
 import com.mueblesStgo.mueblesStgo.services.AutorizacionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,14 +17,13 @@ public class AutorizacionController {
     @Autowired
     AutorizacionService autorizacionService;
 
+    private final Logger log = LoggerFactory.getLogger(AutorizacionController.class);
     @PostMapping("/ingresarAutorizacion")
-    public String guardarAutorizacion(@RequestParam(name="rut") String r, @RequestParam(name="fecha") String f, @RequestParam(name="hora") String h){
-        AutorizacionEntity aut = new AutorizacionEntity();
-        aut.setRutEmpleado(r);
-        LocalDate fechaFormato = autorizacionService.fechaFormato(f);
-        aut.setFechaHoraExtra(fechaFormato);
-        aut.setHorasExtra(Integer.valueOf(h));
-        autorizacionService.guardarAutorizacion(aut);
-        return "Recibido";
+    public @ResponseBody AutorizacionEntity guardarAutorizacion(
+            @RequestBody AutorizacionEntity jsonAutorizacion){
+        log.info("Request to create justification: {}", jsonAutorizacion);
+        System.out.println(jsonAutorizacion.getRutEmpleado() + " " + jsonAutorizacion.getFechaHoraExtra());
+        AutorizacionEntity resultado = autorizacionService.guardarAutorizacion(jsonAutorizacion);
+        return jsonAutorizacion;
     }
 }
